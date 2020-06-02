7b55691c04c41661927500d219c9325253dc3452
hansolo580 <47253266+hansolo580@users.noreply.github.com>
2020-06-02 07:41:39 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# Luke Willey Student ID:001213417
+
+# The main() function is where the program runs - this function calls the rest of the program into play.
+
+
+def main():
+    print("It's working!")
+
+
+main()
